f82bc8277efc81a65988962e19a772506c9affed
bautancredi <bautistatancredi@hotmail.com>
2023-10-11 20:05:59 -0300

    testing out commits

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>RecomenDatos!</Text>
+      <Text>RecomenDatos</Text>
       <StatusBar style="auto" />
     </View>
   );
