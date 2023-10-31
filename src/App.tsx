7a53992f77c982bbb3d82d25587c5d6983e78178
bautancredi <bautistatancredi@hotmail.com>
2023-10-31 13:19:13 -0300

    remove extra stuff, add clerk

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import "react-native-url-polyfill/auto";
-import { useState, useEffect } from "react";
-import { View } from "react-native";
-import { Session } from "@supabase/supabase-js";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import SignUpScreen from "./components/SignUpScreen";
+import tw from "twrnc";
 
-import Profile from "./screens/Profile";
-import Auth from "./screens/Auth";
-import { supabase } from "./supabase";
+const clerkKey = process.env.EXPO_PBULIC_CLERK_PUBLISHABLE_KEY!;
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
   return (
-    <View>
-      {session && session.user ? (
-        <Profile key={session.user.id} session={session} />
-      ) : (
-        <Auth />
-      )}
-    </View>
+    <ClerkProvider
+      publishableKey={"pk_test_Y3V0ZS1yaGluby04LmNsZXJrLmFjY291bnRzLmRldiQ"}
+    >
+      <SafeAreaView style={styles.container}>
+        <SignedIn>
+          <Text style={tw`bg-blue-100`}>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <SignUpScreen />
+        </SignedOut>
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ src/components/buttons/ContinueWithButton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContinueWithButton = () => {
+  return <div>ContinueWithButton</div>;
+};
+
+export default ContinueWithButton;
